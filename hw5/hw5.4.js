6d5a579b4295357f55393b119aa6ab065f335603
dimagutorka <dgutorka@gmail.com>
2025-02-27 17:34:47 +0200

    Fix bugs

--- hw5/hw5.4.js
+++ hw5/hw5.4.js
@@ -1,13 +1,13 @@
-const number = prompt("Enter your Number");
+let primeNumber = prompt("Enter your Number");
 
 
-for(let i = 2; i < number; i++){
+for(let i = 2; i < primeNumber; i++){
 
-    if (number % i === 0) {
+    if (primeNumber % i === 0) {
         console.log("This number is not prime");
         break;
     }
-    else if(i === number - 1) {
+    else if(i === primeNumber - 1) {
         // if it's the last iteration
         console.log("This number is prime");
         break;
---
+++ practise.js
@@ -0,0 +1,4 @@
+let money = 0
+if (money) money = 100; else money = 10;
+
+console.log(money);
