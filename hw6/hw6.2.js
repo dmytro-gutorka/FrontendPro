3124ac637b72190722230d28adff2f4eb01ffcb1
dimagutorka <dgutorka@gmail.com>
2025-04-22 16:37:20 +0300

    Hw18.1

--- hw6/hw6.2.js
+++
@@ -1,9 +0,0 @@
-function arithmeticMean(array){
-    let sum = 0;
-    for (let num of array){
-        if (typeof num === 'number') sum += +num
-    }
-    return sum
-}
-
-console.log(arithmeticMean([10,10,100,1]));
